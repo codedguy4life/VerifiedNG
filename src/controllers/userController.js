20e0b1545c2b5884e7a400990bb629c3417857a7
Codedguy4Life <136790292+codedguy4life@users.noreply.github.com>
2026-03-19 06:22:07 +0100

    Delete src directory
    Src suppose not to be here

--- src/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-// Protected route — only logged in users can access
-const getProfile = (req, res) => {
-  res.status(200).json({
-    message: "You are logged in!",
-    user: req.user, // this came from the middleware
-  });
-};
-
-module.exports = { getProfile };
