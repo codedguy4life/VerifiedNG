059983ce55d9f3da2bd8a83e432672169ebb2a9a
Codedguy4Life <136790292+codedguy4life@users.noreply.github.com>
2026-09-13 03:16:19 +0100

    Expose provider registration endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   register,
+  registerProvider,
   login,
   forgotPassword,
   resetPassword,
 } = require("../controllers/authController");
 
 router.post("/register", register);
+router.post("/register-provider", registerProvider);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
